bae69a882f125b865093f70ec9912e2f7d7953b6
seho <1018715564@qq.com>
2021-12-22 18:05:35 +0800

    🎈 perf: 完善已知问题

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import { ConfigProvider } from "zarm";
 import zhCN from "zarm/lib/config-provider/locale/zh_CN";
+import { config as AmapReactConfig } from "@amap/amap-react";
 import MapComponent from "./pages/MapContainer";
 
 function App() {
+  AmapReactConfig.key = "e10e14f5f4232d3f6bef02d34bb4f716";
+  AmapReactConfig.plugins = ["AMap.ToolBar"];
   return (
     <div className="App">
       <ConfigProvider locale={zhCN}>

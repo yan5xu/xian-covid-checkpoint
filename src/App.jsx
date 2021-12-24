7e81e11441d3bb8994a13e31c8bc5e38f7bb6e08
许长鹏 <lueco.x@gmail.com>
2021-12-24 08:52:51 +0800

    目前h5分支

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import { ConfigProvider } from "zarm";
-import zhCN from "zarm/lib/config-provider/locale/zh_CN";
-import { config as AmapReactConfig } from "@amap/amap-react";
 import MapComponent from "./pages/MapContainer";
 
 function App() {
-  AmapReactConfig.key = "e10e14f5f4232d3f6bef02d34bb4f716";
-  AmapReactConfig.plugins = ["AMap.ToolBar"];
   return (
     <div className="App">
-      <ConfigProvider locale={zhCN}>
-        <MapComponent></MapComponent>
-      </ConfigProvider>
+      <MapComponent></MapComponent>
     </div>
   );
 }
--- src/pages/MapContainer/index.jsx
+++ src/pages/MapContainer/index.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
-import { Amap, useAmap, loadPlugins } from "@amap/amap-react";
+import React, { useEffect, useState } from "react";
+import AMapLoader from "@amap/amap-jsapi-loader";
 import "./index.scss";
-import axios from "axios";
-import { MakerPopup } from "./components/MarkerPopup";
+import axios, { AxiosInstance } from "axios";
+
+const defaultSize = 0;
 
 const getCheckPoint = async () => {
   const res = await axios.post("https://ywhasura.xzllo.com/v1/graphql", {
     query:
-      "query getCheckPoint {\n  check_point {\n    id\n    name\n    position\n    type\n  }\n}\n",
+      "query getCheckPoint {\n  check_point {\n    id\n    name\n    position\n    type\n info\n  }\n}\n",
     variables: null,
     operationName: "getCheckPoint",
   });
   return res.data.data.check_point.map((one) => ({
     title: one.name,
     position: one.position.split(",").map((str) => parseFloat(str)),
+    type: one.type,
+    info: one.info,
   }));
 };
 
-// 返回定位坐标
-export const getGeoLocation = async () => {
-  return new Promise(async (resolve, error) => {
-    const AMap = await loadPlugins("AMap.Geolocation");
-    const geolocation = new AMap.Geolocation({
+const getLocation = (AMap, _map) => {
+  AMap.plugin("AMap.Geolocation", function () {
+    var geolocation = new AMap.Geolocation({
       enableHighAccuracy: true, //是否使用高精度定位，默认:true
       timeout: 10000, //超过10秒后停止定位，默认：5s
       buttonPosition: "RB", //定位按钮的停靠位置
@@ -29,162 +30,197 @@ export const getGeoLocation = async () => {
       zoomToAccuracy: true, //定位成功后是否自动调整地图视野到定位点
       noGeoLocation: 0,
     });
-
-    geolocation.getCurrentPosition((status, result) => {
-      if (status === "complete") {
-        resolve(result);
+    _map.addControl(geolocation);
+    geolocation.getCurrentPosition(function (status, result) {
+      if (status == "complete") {
+        onComplete(result);
       } else {
-        error(result);
+        onError(result);
       }
     });
   });
+  //解析定位结果
+  function onComplete(data) {
+    console.log(data);
+  }
+  //解析定位错误信息
+  function onError(data) {
+    console.log(data);
+  }
 };
 
-// 绘制海量点LabelMarker包装组件
-const ManyPoints = (props) => {
-  const { data, click } = props;
-  const map = useAmap();
-  const $layer = useRef();
-
-  const handleClickPoint = (e) => {
-    const {
-      data: {
-        data: { position, name },
+const Map = () => {
+  const [map, setMap] = useState();
+  const [points, setPoints] = useState([]);
+  useEffect(() => {
+    AMapLoader.load({
+      key: "e10e14f5f4232d3f6bef02d34bb4f716", //需要设置您申请的key
+      version: "2.0",
+      plugins: ["AMap.ToolBar"],
+      AMapUI: {
+        version: "1.1",
+        plugins: [],
       },
-    } = e;
-    click &&
-      click({
-        position,
-        name,
-      });
-  };
+      Loca: {
+        version: "2.0.0",
+      },
+    })
+      .then(async (AMap) => {
+        // 渲染marker存放
+        const markerArr = [];
+        // 加载点
+        const point = await getCheckPoint();
+        const _map = new AMap.Map("mapcontainer", {
+          viewMode: "3D",
+          zoom: defaultSize,
+          zooms: [2, 22],
+          visible: true, //是否可见
+          center: [108.94703, 34.25943], // 初始点
+        });
 
-  useEffect(() => {
-    if (!map) return;
-    if (!$layer.current) {
-      $layer.current = new window.AMap.LabelsLayer({
-        zooms: [3, 20],
-        zIndex: 1000,
-        collision: false,
-      });
-      map.add($layer.current);
-    }
-    const layer = $layer.current;
-    const markers = data.map((d) => {
-      const _maker = new window.AMap.LabelMarker({
-        position: d.position,
-        icon: {
-          type: "image",
-          image:
-            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAfCAYAAAAFkva3AAACd0lEQVR42qWU3UuTYRjG34iiw+gk6CAp2XRqsyazWRuz8c656UF/QV8URhQSNaMP5kzRudQ5Zy3zD+i0gygiZ44oogjDJJBqjMTS1Gxzm1s17+4rYmBzbXt38OPhue/r+jEG7yMQURoWZ2QHY2XGmBkm8ff0Y479er01F3N3eBPTzsQs7gQ1eJPUOETUOMzwiTvm2COH/LqyekdoK+Mz98ep4Q4XhzODPXKcH2W2rZGZupY2MiP17hWycDBXkOfec2ZzSlbXuWg39UXJPER5gx76BJmxY347EzPx/1HHy3xBD314BLF97qLYs0zG26uSQZ89VsHQ9mVUHFgh0ZuUDPrwCIfsM3OGmz/I4P0lGfTZMyvU2qaTtbd+UqHAI+ivBZf1gwnSs10q6LMnLOiuBsZ1rgjpeCCZ/gix542gvfLerXXM00FPXDJaxwKxxyMcuDSlqbEFSeNZkQz68Pz5Ampa3o3t75yl6oFY3qCHfupz0lgnS5i42hUmtTuaM8ijh35KBqovTLSp7Z+oqj+aM8ijl/YEqc+Pb2ECqq6vtM8VyQpyyKOXJgNVza8Pqy5PkbJvOSvIIZ/xpQWqcy9HKvnnV/SGM4I953zI/1e29+wLJZMs7w1RJrBHLqsMVJ55drfCFiBFz/c0MMceuZxkytNPy5hV+Y0l+hfMsc9ZBvY0+e+VtgZJ5vyWAnfMsc9LVn7qiahofkXFzsUUuGOet6zspG8D87G44zPt7l4gnLhjnrcMKE48tsmsb6mIXwWcfG/FXJKs9PijUnmTn3byE4UTd8kyUHLs4Yei69OEE/eCZPKjDwZ3tUwSzoJlsiP3RSbEGLNlfwPf80fNP6DL+QAAAABJRU5ErkJggg==",
-          size: [26, 40],
-          anchor: "bottom-center",
-        },
-        text: {
-          content: d.title,
-          style: {
-            fontSize: 14,
-            fillColor: "#000000",
-            padding: [20, 18],
-            backgroundColor: "#FFFFFF",
-          },
-        },
-      });
-      _maker.on("click", handleClickPoint);
-      return _maker;
-    });
-    layer.add(markers);
+        //缩放级别begin=============
+        AMap.Event.addListener(_map, "zoomend", function () {
+          const sfjb = _map.getZoom();
+          if (sfjb < defaultSize) {
+            for (let i = 0; i < markerArr.length; i += 1) {
+              markerArr[i].hide();
+            }
+          } else {
+            for (let i = 0; i < markerArr.length; i += 1) {
+              markerArr[i].show();
+            }
+          }
+        });
+        //缩放级别end=============
 
-    return () => {
-      layer.remove(markers);
-    };
-  }, [map, data]);
+        const refresh = function () {
+          //得到屏幕可视范围的坐标，画出矩形
 
-  useEffect(() => {
-    return () => {
-      if ($layer.current) {
-        $layer.current.setMap(null);
-      }
-    };
+          const tmapBounds = _map.getBounds();
+
+          const southWest = new AMap.LngLat(
+            tmapBounds.southWest.lng,
+            tmapBounds.southWest.lat
+          );
+          const northEast = new AMap.LngLat(
+            tmapBounds.northEast.lng,
+            tmapBounds.northEast.lat
+          );
+
+          const bounds = new AMap.Bounds(southWest, northEast);
+          const rectangle = new AMap.Rectangle({
+            map: _map,
+            bounds: bounds,
+            strokeColor: "#FFFFFF",
+            strokeWeight: 1,
+            strokeOpacity: 0,
+            fillOpacity: 0,
+            zIndex: 0,
+            bubble: true,
+          });
+
+          const blueIcon = new AMap.Icon({
+            size: new AMap.Size(19, 32), // 图标尺寸
+            image: "//webapi.amap.com/theme/v1.3/markers/b/mark_bs.png", // Icon的图像
+            imageSize: new AMap.Size(19, 32), // 根据所设置的大小拉伸或压缩图片
+          });
+
+          const retIcon = new AMap.Icon({
+            size: new AMap.Size(19, 32), // 图标尺寸
+            image:
+              "//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-red.png", // Icon的图像
+            imageSize: new AMap.Size(19, 32), // 根据所设置的大小拉伸或压缩图片
+          });
+
+          for (let i = 0, marker; i < point.length; i++) {
+            var myLngLat = new AMap.LngLat(
+              point[i].position[0],
+              point[i].position[1]
+            );
+            // 标点坐标在屏幕显示范围内且图层大于13   地图缩放比例过大时，会将所有隐藏
+            if (rectangle.contains(myLngLat) && _map.getZoom() > defaultSize) {
+              //如果点在矩形内则输出
+              marker = new AMap.Marker({
+                map: _map,
+                position: point[i].position,
+                title: point[i].title,
+                icon: point[i].type === "24小时" ? retIcon : blueIcon,
+              });
+              marker.content =
+                point[i].type === "24小时"
+                  ? `<div>${point[i].title}</div>${point[i].info.phone}`
+                  : `<div>${point[i].title}</div>`;
+              marker.on("click", markerClick);
+              // 满足条件后将标点放入标点数组
+              markerArr.push(marker);
+            }
+          }
+        };
+        //地图平移结束begin=============
+        AMap.Event.addListener(_map, "moveend", function () {
+          _map.remove(markerArr); //只删除marker点组
+          refresh();
+        });
+        //地图平移结束end=============
+
+        // 信息窗体
+        var infoWindow = new AMap.InfoWindow({
+          isCustom: true, //使用自定义窗体
+          closeWhenClickMap: true, // 点击地图关闭信息窗口
+          offset: new AMap.Pixel(0, -50),
+        });
+        // marker点击
+        function markerClick(e) {
+          // console.log(e);
+          const content = `<div class="marker-content">${e.target.content}</div>`;
+          infoWindow.setContent(content); //必须要用setContent方法
+          infoWindow.open(_map, e.target.getPosition());
+        }
+        setPoints(point);
+        // !加点
+        _map.add(markerArr);
+        getLocation(AMap, _map);
+        setMap(_map);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }, []);
 
-  return null;
-};
+  const jumpTo = () => {
+    window.location.href =
+      "https://cloud.seatable.cn/dtable/forms/30f44612-f350-4ff2-b928-6fce6f9aaac4/";
+  };
 
-const Main = () => {
-  const [points, setPoints] = useState([]);
-  // 存储本身
-  const [mapSelf, setMapSelf] = useState();
-  // 存储当前活动点信息（被点击）
-  const [activePoint, setActivePoint] = useState();
-  // 存储活动点信息的被缓存坐标
-  const makerContentPosition = useMemo(
-    () => (activePoint ? activePoint.position : [108.94703, 34.25943]),
-    [activePoint]
-  );
-  // 当前详情popup是否显示
-  const [detailPopupVisible, setDetailPopupVisible] = useState(false);
-  const map = useRef();
-
-  setTimeout(() => {
-    if (map.current && !mapSelf) {
-      setMapSelf(map.current);
-    }
-  }, 1000);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(async () => {
-    if (mapSelf) {
-      try {
-        const selfLocation = await getGeoLocation(mapSelf);
-        mapSelf.setCenter(selfLocation.position);
-      } catch {
-        mapSelf.setCenter([108.94703, 34.25943]);
-      }
-      mapSelf.setZoom(17);
-      // 加载点
-      const point = await getCheckPoint();
-      setPoints(point);
-    }
-  }, [mapSelf]);
+  const isInWechatMP = () => {
+    return (
+      (navigator.userAgent.match(/micromessenger/i) &&
+        navigator.userAgent.match(/miniprogram/i)) ||
+      window.__wxjs_environment === "miniprogram"
+    );
+  };
 
   return (
     <div className="home_div">
       <div className="map-title">
         <h4>
-          共收录检测点{points.length}个，检测点实时动态功能正在开发
+          红色为24h检测点，蓝色为临时检测点
           <br />
-          点击图标可查看检测点详情以及其他信息
+          大家优先前往自家小区检测点，其次为24h检测点
           <br />
           其他请联系wx: cplife
         </h4>
       </div>
-      <Amap
-        ref={map}
-        viewMode="3D"
-        center={makerContentPosition}
-        zooms={[2, 22]}
-      >
-        {/* 绘制点的组件 */}
-        <ManyPoints
-          data={points}
-          click={(e) => {
-            setActivePoint(e);
-            setDetailPopupVisible(true);
-          }}
-        ></ManyPoints>
-        {/* 绘制详情的组件 */}
-        <MakerPopup
-          visible={detailPopupVisible}
-          data={activePoint}
-          getGeoLocation={getGeoLocation}
-          onClose={() => {
-            setDetailPopupVisible(false);
-          }}
-        ></MakerPopup>
-      </Amap>
+      <div id="mapcontainer" className="map" style={{ height: "100%" }} />
+      {isInWechatMP() ? (
+        <></>
+      ) : (
+        <button className="addInfo button-yellow" onClick={jumpTo}>
+          反馈信息
+        </button>
+      )}
     </div>
   );
 };
 
-export default Main;
+export default Map;
